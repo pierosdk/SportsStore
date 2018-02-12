62ca36b931bedd118c0cb8210de6c783426db619
pierosdk <pierosdk@hotmail.com>
2018-02-12 18:18:14 +0100

    plug-in code formatter carica gli elementi mancanti

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsStore.Models
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines the <see cref="FakeProductRepository" />
+    /// </summary>
+    public class FakeProductRepository : IProductRepository
+    {
+        /// <summary>
+        /// Gets the Products
+        /// </summary>
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Surf board", Price = 179 },
+            new Product { Name = "Running shoes", Price = 95 }
+         }.AsQueryable<Product>();
+    }
+}
